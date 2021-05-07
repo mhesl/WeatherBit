02ff346de7145da1c26e4f221cbc596215d78b0f
mohamadeslami <mohammadeslami1379@yahoo.com>
2021-05-07 23:00:57 +0430

    showing current and hourly weather finished

--- WeatherBit/WeatherViewController.swift
+++ WeatherBit/WeatherViewController.swift
@@ -29,6 +29,40 @@ class WeatherViewController: UIViewController {
         }
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        let weatherView = WeatherView()
+        
+        weatherView.frame = CGRect(x: 0, y: 0, width: scrollView.bounds.width, height: scrollView.bounds.height)
+        scrollView.addSubview(weatherView)
+        getCurrentWeather(weatherView: weatherView)
+        getWeeklyWeather(weatherView: weatherView)
+        getHourlyWeather(weatherView: weatherView)
+
+    }
+    
+    private func getCurrentWeather(weatherView: WeatherView) {
+        weatherView.currentWeather = CurrentWeather()
+        weatherView.currentWeather.getCurrentWeather { (success) in
+            weatherView.refreshData()
+        }
+        
+    }
+    
+    private func getWeeklyWeather(weatherView: WeatherView) {
+        WeeklyForecast.downloadWeaklyForcastWeather { (weatherForecast) in
+            weatherView.weeklyWeatherForecastData = weatherForecast
+            weatherView.tableView.reloadData()
+        }
+    }
+    
+    private func getHourlyWeather(weatherView: WeatherView) {
+        HourlyForecast.downloadDailyForecastWeather { (weatherForecast) in
+            weatherView.dailyWeatherForecastData = weatherForecast
+            weatherView.hourlyWeatherCollectionView.reloadData()
+        }
+    }
+    
 
 
 }
